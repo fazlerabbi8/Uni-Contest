422171d436ae5af3fd23328e5f7a63c49f31e1bd
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-13 16:34:10 +0600

    nim game,where if xor equal 0 then second player will win if not 0 the first player is win.

---
+++ Game_Theory/E.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int nim = 0;
+
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        nim ^= x;
+    }
+
+    if(nim == 0){
+        cout << "Second\n";
+    }else{
+        cout << "First\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+}
